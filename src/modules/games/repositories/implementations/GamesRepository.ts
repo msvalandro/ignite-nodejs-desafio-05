e036a48f270a569f374b20f2b92793a50c8d4875
Matheus Valandro <msvalandro@gmail.com>
2021-11-30 20:30:56 -0300

    add tests

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,24 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const games = await this.repository
+      .createQueryBuilder("game")
+      .where("game.title ilike :title", { title: `%${param}%` })
+      .getMany()
+
+    return games;
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT COUNT(id) FROM games`
+    );
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return this.repository.createQueryBuilder("user_game")
+      .relation(Game, "users")
+      .of(id)
+      .loadMany()
   }
 }
